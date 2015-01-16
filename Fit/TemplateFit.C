ffe82665a650bbfcdae641bdf403e62402d70991
nadjieh <ajafari@cern.ch>
2015-01-16 20:03:36 +0100

    some comments ...

--- Fit/TemplateFit.C
+++ Fit/TemplateFit.C
@@ -10,9 +10,13 @@ void TemplateFit(){
     //The variable 
     double lower = -1.5; //adjust accordingly
     double upper = 1.5; //adjust accordingly
-    RooRealVar var("var","var", lower, upper);
+    RooRealVar var("charge","lepton charge", lower, upper);
 
     //Get histograms for data, signal and backgrounds
+    //The current modeling discriminates between W+jets and other backgrounds. 
+    //It could be different :)
+    
+    //Remember to adjust all the names for histograms and input files
 	TFile * f = TFile::Open("Charge.root");
 	
 	TH1D * dataHist =  (TH1D*)f->Get("Data");
@@ -31,15 +35,18 @@ void TemplateFit(){
     	cout << i <<endl;
     	bkgHist->Add((TH1D*)f->Get(list->At(i)->GetName()));
     }
-    
     double nBkg = bkgHist->Integral();	
     
+
+	//Declaration of variables
     RooRealVar nbkg("nbkg","nbkg", nBkg, 0,2*nBkg);
     RooRealVar nsig("nsig","nsig", nSignal, 0,2*nSignal);  
     RooRealVar nwjets("nwjets","nwjets", nWjets, 0,2*nWjets); 
     
+    //Data declaration
     RooDataHist data("data","data",var, dataHist);
     
+    //Model construction
     RooDataHist sig("sig","sig",var, signalHist);
     RooHistPdf sigPdf("sigPdf","sigPdf",var, sig);
     
@@ -50,9 +57,11 @@ void TemplateFit(){
     RooHistPdf bkgPdf("bkgPdf","bkgPdf",var, bkg);
     
     RooAddPdf model("model","model", RooArgList(sigPdf,wjetsPdf,bkgPdf),RooArgList(nsig,nwjets,nbkg));    
-     
+    
+    //Fitting 
     model.fitTo(data);    
     
+    //Visualizing
     RooPlot * fr = var.frame();
     data.plotOn(fr);
     model.plotOn(fr);
@@ -62,8 +71,13 @@ void TemplateFit(){
         RooFit::LineStyle(kDashed),RooFit::LineColor(kRed));      
     model.plotOn(fr,RooFit::Components(bkgPdf), 
         RooFit::LineStyle(kDashed),RooFit::LineColor(kGreen+3));               
+    TCanvas c;
+    c.cd();
     fr->Draw(); 
+    c.SaveAs("charge.C");
     
+    //Print out
+    //You can find more clever method to have the same on your screen :)
 	cout<<"Signal yield: "<<nsig.getVal() <<" +/- "<<nsig.getError() <<endl;
     cout<<"Bkg yield: "<<nbkg.getVal() <<" +/- "<<nbkg.getError() <<endl;
     cout<<"Wjets yield: "<<nwjets.getVal() <<" +/- "<<nwjets.getError() <<endl;
