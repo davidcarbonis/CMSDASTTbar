78142c96a31b0403b74b1e914b36e800c5febcce
Andrey Popov <rainfinder@gmail.com>
2015-01-19 01:03:06 +0100

    Update the script to setup the environment
    It will work both at Bari and lxplus.

--- env.sh
+++ env.sh
@@ -1,1 +1,9 @@
 export CMS_DAS_TTbar_INSTALL=`pwd`
+
+gcc_version="4.9.1"
+source /afs/cern.ch/sw/lcg/external/gcc/$gcc_version/x86_64-slc6/setup.sh ""
+unset gcc_version
+
+root_version="5.34.24"
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/$root_version/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+unset root_version
